ddfcf8d5e4a7a2250e0eb89f617362513a5e23f1
Kamil Chomiak <kchomiak@outlook.com>
2022-10-17 23:16:48 -0400

    initial commit

---
+++ animations.js
@@ -0,0 +1,388 @@
+class K_letter extends mojs.CustomShape {
+  getShape() {
+    return '<path d="M 0 24.032 L 0 0.96 A 0.984 0.984 0 0 1 0.063 0.602 A 0.895 0.895 0 0 1 0.288 0.272 Q 0.576 0 0.96 0 Q 1.376 0 1.648 0.272 A 0.919 0.919 0 0 1 1.918 0.895 A 1.222 1.222 0 0 1 1.92 0.96 L 1.92 12.704 L 14.336 0.288 A 1.093 1.093 0 0 1 14.489 0.159 A 0.814 0.814 0 0 1 14.672 0.064 Q 14.848 0 15.072 0 A 1.033 1.033 0 0 1 15.384 0.045 A 0.83 0.83 0 0 1 15.744 0.272 Q 16 0.544 16.032 0.928 A 0.837 0.837 0 0 1 16.035 0.998 Q 16.035 1.344 15.744 1.664 L 6.56 10.848 L 15.84 23.456 Q 16.096 23.808 16.032 24.176 A 1.089 1.089 0 0 1 15.925 24.499 A 0.932 0.932 0 0 1 15.712 24.768 Q 15.456 24.992 15.104 24.992 A 1.13 1.13 0 0 1 14.739 24.936 A 0.918 0.918 0 0 1 14.272 24.576 L 5.152 12.256 L 1.92 15.488 L 1.92 24.032 Q 1.92 24.448 1.648 24.72 A 0.919 0.919 0 0 1 1.025 24.99 A 1.222 1.222 0 0 1 0.96 24.992 Q 0.576 24.992 0.288 24.72 A 0.9 0.9 0 0 1 0.001 24.064 A 1.178 1.178 0 0 1 0 24.032 Z" vector-effect="non-scaling-stroke"/>';
+  }
+}
+class A_letter extends mojs.CustomShape {
+  getShape() {
+    return '<path d="M 17.888 8.96 A 10.712 10.712 0 0 0 17.86 8.173 A 8.833 8.833 0 0 0 16.72 4.368 Q 15.552 2.336 13.536 1.168 A 8.398 8.398 0 0 0 11.738 0.391 A 9.601 9.601 0 0 0 8.96 0 A 10.712 10.712 0 0 0 8.173 0.029 A 8.833 8.833 0 0 0 4.368 1.168 Q 2.336 2.336 1.168 4.368 Q 0 6.4 0 8.96 Q 0 11.488 1.168 13.52 Q 2.336 15.552 4.368 16.72 Q 6.4 17.888 8.96 17.888 A 10.06 10.06 0 0 0 10.027 17.833 A 8.099 8.099 0 0 0 13.12 16.88 Q 14.944 15.872 16.016 14.144 A 6.991 6.991 0 0 0 16.648 12.844 A 7.564 7.564 0 0 0 17.088 10.24 L 17.888 8.96 L 16.576 8.96 L 15.968 10.88 L 15.968 16.736 A 1.222 1.222 0 0 0 15.97 16.801 A 0.919 0.919 0 0 0 16.24 17.424 A 0.892 0.892 0 0 0 16.64 17.656 A 1.197 1.197 0 0 0 16.96 17.696 A 1.176 1.176 0 0 0 17.075 17.691 A 0.853 0.853 0 0 0 17.632 17.424 A 0.907 0.907 0 0 0 17.84 17.071 A 1.108 1.108 0 0 0 17.888 16.736 L 17.888 8.96 Z M 8.96 16.096 A 7.357 7.357 0 0 0 11.174 15.772 A 6.497 6.497 0 0 0 12.56 15.152 Q 14.144 14.208 15.056 12.592 A 7.123 7.123 0 0 0 15.953 9.473 A 8.588 8.588 0 0 0 15.968 8.96 A 7.868 7.868 0 0 0 15.663 6.737 A 6.831 6.831 0 0 0 15.056 5.296 Q 14.144 3.68 12.56 2.736 A 6.729 6.729 0 0 0 9.58 1.815 A 8.205 8.205 0 0 0 8.96 1.792 A 7.171 7.171 0 0 0 6.468 2.218 A 6.674 6.674 0 0 0 5.376 2.736 Q 3.776 3.68 2.848 5.296 A 7.027 7.027 0 0 0 1.948 8.269 A 8.58 8.58 0 0 0 1.92 8.96 Q 1.92 10.976 2.848 12.592 Q 3.776 14.208 5.376 15.152 A 6.864 6.864 0 0 0 8.714 16.093 A 8.192 8.192 0 0 0 8.96 16.096 Z" vector-effect="non-scaling-stroke"/>';
+  }
+}
+class M_letter extends mojs.CustomShape {
+  getShape() {
+    return '<path d="M 23.136 16.768 L 23.136 7.52 A 7.668 7.668 0 0 0 22.938 5.732 A 5.464 5.464 0 0 0 21.84 3.488 A 4.13 4.13 0 0 0 18.847 1.934 A 5.243 5.243 0 0 0 18.464 1.92 A 5.46 5.46 0 0 0 16.746 2.179 A 4.536 4.536 0 0 0 14.864 3.376 A 4.692 4.692 0 0 0 13.657 5.757 Q 13.533 6.377 13.533 7.082 A 8.268 8.268 0 0 0 13.536 7.296 L 11.616 7.296 A 9.749 9.749 0 0 1 11.859 5.353 A 7.45 7.45 0 0 1 12.576 3.456 Q 13.472 1.824 15.008 0.912 A 6.555 6.555 0 0 1 18.107 0.008 A 7.866 7.866 0 0 1 18.464 0 A 6.708 6.708 0 0 1 20.396 0.269 A 5.717 5.717 0 0 1 21.888 0.96 A 6.539 6.539 0 0 1 24.029 3.279 A 7.868 7.868 0 0 1 24.208 3.616 A 7.958 7.958 0 0 1 24.956 6.063 A 10.259 10.259 0 0 1 25.056 7.52 L 25.056 16.768 A 1.108 1.108 0 0 1 25.007 17.103 A 0.907 0.907 0 0 1 24.8 17.456 Q 24.544 17.728 24.096 17.728 Q 23.68 17.728 23.408 17.456 A 0.919 0.919 0 0 1 23.138 16.833 A 1.222 1.222 0 0 1 23.136 16.768 Z M 11.616 16.768 L 11.616 7.52 A 7.668 7.668 0 0 0 11.418 5.732 A 5.464 5.464 0 0 0 10.32 3.488 A 4.13 4.13 0 0 0 7.327 1.934 A 5.243 5.243 0 0 0 6.944 1.92 A 5.659 5.659 0 0 0 5.223 2.171 A 4.626 4.626 0 0 0 3.328 3.328 A 4.747 4.747 0 0 0 1.962 6.231 A 6.5 6.5 0 0 0 1.92 6.976 L 0.32 6.976 Q 0.416 4.928 1.296 3.36 Q 2.176 1.792 3.648 0.896 Q 5.12 0 6.944 0 A 6.708 6.708 0 0 1 8.876 0.269 A 5.717 5.717 0 0 1 10.368 0.96 A 6.539 6.539 0 0 1 12.509 3.279 A 7.868 7.868 0 0 1 12.688 3.616 A 7.958 7.958 0 0 1 13.436 6.063 A 10.259 10.259 0 0 1 13.536 7.52 L 13.536 16.768 A 1.108 1.108 0 0 1 13.487 17.103 A 0.907 0.907 0 0 1 13.28 17.456 Q 13.024 17.728 12.576 17.728 Q 12.16 17.728 11.888 17.456 A 0.919 0.919 0 0 1 11.618 16.833 A 1.222 1.222 0 0 1 11.616 16.768 Z M 0 16.768 L 0 1.184 A 1.188 1.188 0 0 1 0.035 0.887 A 0.831 0.831 0 0 1 0.272 0.48 Q 0.544 0.224 0.96 0.224 Q 1.408 0.224 1.664 0.48 A 0.847 0.847 0 0 1 1.89 0.899 A 1.27 1.27 0 0 1 1.92 1.184 L 1.92 16.768 A 1.108 1.108 0 0 1 1.871 17.103 A 0.907 0.907 0 0 1 1.664 17.456 Q 1.408 17.728 0.96 17.728 Q 0.544 17.728 0.272 17.456 A 0.919 0.919 0 0 1 0.002 16.833 A 1.222 1.222 0 0 1 0 16.768 Z" vector-effect="non-scaling-stroke"/>';
+  }
+}
+class I_letter extends mojs.CustomShape {
+  getShape() {
+    return '<path d="M 0.544 23.2 L 0.544 7.616 A 1.188 1.188 0 0 1 0.579 7.319 A 0.831 0.831 0 0 1 0.816 6.912 Q 1.088 6.656 1.504 6.656 Q 1.952 6.656 2.208 6.912 A 0.847 0.847 0 0 1 2.434 7.331 A 1.27 1.27 0 0 1 2.464 7.616 L 2.464 23.2 A 1.108 1.108 0 0 1 2.416 23.535 A 0.907 0.907 0 0 1 2.208 23.888 Q 1.952 24.16 1.504 24.16 Q 1.088 24.16 0.816 23.888 A 0.919 0.919 0 0 1 0.546 23.265 A 1.222 1.222 0 0 1 0.544 23.2 Z M 0.446 2.557 A 1.416 1.416 0 0 0 1.472 2.976 A 1.88 1.88 0 0 0 1.492 2.976 A 1.452 1.452 0 0 0 2.544 2.544 Q 2.976 2.112 2.976 1.472 Q 2.976 0.736 2.512 0.368 A 1.984 1.984 0 0 0 2.41 0.292 A 1.559 1.559 0 0 0 1.504 0 Q 0.928 0 0.464 0.368 A 1.178 1.178 0 0 0 0.049 1.033 A 1.85 1.85 0 0 0 0 1.472 A 1.88 1.88 0 0 0 0 1.492 A 1.452 1.452 0 0 0 0.432 2.544 A 1.725 1.725 0 0 0 0.446 2.557 Z" vector-effect="non-scaling-stroke"/>';
+  }
+}
+class L_letter extends mojs.CustomShape {
+  getShape() {
+    return '<path d="M 4.8 24.992 L 4.16 24.992 Q 2.944 24.992 2 24.32 A 4.258 4.258 0 0 1 0.779 22.958 A 5.315 5.315 0 0 1 0.528 22.464 A 5.944 5.944 0 0 1 0.094 20.967 A 8.018 8.018 0 0 1 0 19.712 L 0 0.96 A 1.188 1.188 0 0 1 0.035 0.663 A 0.831 0.831 0 0 1 0.272 0.256 A 0.933 0.933 0 0 1 0.834 0.006 A 1.25 1.25 0 0 1 0.96 0 Q 1.408 0 1.664 0.256 A 0.847 0.847 0 0 1 1.89 0.675 A 1.27 1.27 0 0 1 1.92 0.96 L 1.92 19.712 A 5.703 5.703 0 0 0 2.008 20.742 Q 2.154 21.538 2.544 22.128 Q 3.168 23.072 4.16 23.072 L 4.96 23.072 Q 5.312 23.072 5.536 23.328 Q 5.76 23.584 5.76 24.032 A 1.108 1.108 0 0 1 5.711 24.367 A 0.907 0.907 0 0 1 5.504 24.72 Q 5.248 24.992 4.8 24.992 Z" vector-effect="non-scaling-stroke"/>';
+  }
+}
+class C_letter extends mojs.CustomShape {
+  getShape() {
+    return '<path d="M 12.512 25.632 Q 9.952 25.632 7.696 24.624 Q 5.44 23.616 3.712 21.84 Q 1.984 20.064 0.992 17.744 Q 0 15.424 0 12.8 Q 0 10.208 0.96 7.888 Q 1.92 5.568 3.632 3.792 Q 5.344 2.016 7.616 1.008 A 11.916 11.916 0 0 1 12.43 0 A 13.624 13.624 0 0 1 12.512 0 Q 14.72 0 16.704 0.704 A 11.971 11.971 0 0 1 20.09 2.585 A 13.643 13.643 0 0 1 20.416 2.848 A 0.727 0.727 0 0 1 20.7 3.381 A 1.031 1.031 0 0 1 20.704 3.472 Q 20.704 3.872 20.448 4.176 A 0.838 0.838 0 0 1 19.88 4.473 A 1.198 1.198 0 0 1 19.744 4.48 A 0.718 0.718 0 0 1 19.508 4.439 A 0.819 0.819 0 0 1 19.488 4.432 Q 19.36 4.384 19.232 4.304 Q 19.134 4.243 19.017 4.154 A 4.362 4.362 0 0 1 18.944 4.096 Q 17.664 2.944 16.032 2.432 Q 14.4 1.92 12.512 1.92 Q 10.368 1.92 8.448 2.8 Q 6.528 3.68 5.056 5.2 Q 3.584 6.72 2.752 8.672 Q 1.92 10.624 1.92 12.8 Q 1.92 15.04 2.752 17.024 Q 3.584 19.008 5.04 20.512 Q 6.496 22.016 8.416 22.864 Q 10.336 23.712 12.512 23.712 Q 14.4 23.712 16.048 23.136 A 9.996 9.996 0 0 0 18.91 21.557 A 11.392 11.392 0 0 0 19.136 21.376 Q 19.382 21.162 19.715 21.211 A 0.938 0.938 0 0 1 19.744 21.216 Q 20.096 21.28 20.352 21.536 A 0.86 0.86 0 0 1 20.596 22.034 A 1.198 1.198 0 0 1 20.608 22.208 A 2.006 2.006 0 0 1 20.598 22.411 A 1.571 1.571 0 0 1 20.576 22.56 Q 20.55 22.694 20.434 22.827 A 1.024 1.024 0 0 1 20.384 22.88 Q 18.816 24.448 16.752 25.04 Q 14.688 25.632 12.512 25.632 Z" vector-effect="non-scaling-stroke"/>';
+  }
+}
+class H_letter extends mojs.CustomShape {
+  getShape() {
+    return '<path d="M 14.176 24.032 L 14.176 15.424 Q 14.176 13.683 13.622 12.443 A 4.952 4.952 0 0 0 13.36 11.936 Q 12.544 10.56 11.152 9.872 A 6.645 6.645 0 0 0 8.961 9.238 A 8.279 8.279 0 0 0 8 9.184 A 7.812 7.812 0 0 0 6.191 9.386 A 6.476 6.476 0 0 0 4.896 9.84 A 5.733 5.733 0 0 0 3.417 10.839 A 5.134 5.134 0 0 0 2.72 11.648 Q 1.92 12.8 1.92 14.24 L 0.32 14.24 Q 0.448 12.256 1.504 10.672 Q 2.56 9.088 4.256 8.176 A 7.72 7.72 0 0 1 7.745 7.268 A 9.054 9.054 0 0 1 8 7.264 A 9.688 9.688 0 0 1 10.445 7.563 A 8.262 8.262 0 0 1 12.144 8.208 Q 13.984 9.152 15.04 10.96 A 7.389 7.389 0 0 1 15.864 13.12 Q 16.067 14.047 16.092 15.116 A 12.992 12.992 0 0 1 16.096 15.424 L 16.096 24.032 Q 16.096 24.448 15.824 24.72 A 0.919 0.919 0 0 1 15.201 24.99 A 1.222 1.222 0 0 1 15.136 24.992 Q 14.72 24.992 14.448 24.72 A 0.919 0.919 0 0 1 14.178 24.097 A 1.222 1.222 0 0 1 14.176 24.032 Z M 0 24.032 L 0 8.448 A 1.188 1.188 0 0 1 0.035 8.151 A 0.831 0.831 0 0 1 0.272 7.744 Q 0.544 7.488 0.96 7.488 Q 1.408 7.488 1.664 7.744 A 0.847 0.847 0 0 1 1.89 8.163 A 1.27 1.27 0 0 1 1.92 8.448 L 1.92 24.032 A 1.108 1.108 0 0 1 1.871 24.367 A 0.907 0.907 0 0 1 1.664 24.72 Q 1.408 24.992 0.96 24.992 Q 0.544 24.992 0.272 24.72 A 0.919 0.919 0 0 1 0.002 24.097 A 1.222 1.222 0 0 1 0 24.032 Z M 0 16.352 L 0 0.96 A 1.188 1.188 0 0 1 0.035 0.663 A 0.831 0.831 0 0 1 0.272 0.256 A 0.933 0.933 0 0 1 0.834 0.006 A 1.25 1.25 0 0 1 0.96 0 Q 1.408 0 1.664 0.256 A 0.847 0.847 0 0 1 1.89 0.675 A 1.27 1.27 0 0 1 1.92 0.96 L 1.92 16.352 A 1.108 1.108 0 0 1 1.871 16.687 A 0.907 0.907 0 0 1 1.664 17.04 Q 1.408 17.312 0.96 17.312 Q 0.544 17.312 0.272 17.04 A 0.919 0.919 0 0 1 0.002 16.417 A 1.222 1.222 0 0 1 0 16.352 Z" vector-effect="non-scaling-stroke"/>';
+  }
+}
+class O_letter extends mojs.CustomShape {
+  getShape() {
+    return '<path d="M 8.96 17.888 Q 6.4 17.888 4.368 16.72 Q 2.336 15.552 1.168 13.52 Q 0 11.488 0 8.96 Q 0 6.4 1.168 4.368 Q 2.336 2.336 4.368 1.168 A 8.833 8.833 0 0 1 8.173 0.029 A 10.712 10.712 0 0 1 8.96 0 A 9.601 9.601 0 0 1 11.738 0.391 A 8.398 8.398 0 0 1 13.536 1.168 Q 15.552 2.336 16.72 4.368 A 8.833 8.833 0 0 1 17.86 8.173 A 10.712 10.712 0 0 1 17.888 8.96 A 9.405 9.405 0 0 1 17.409 11.996 A 8.561 8.561 0 0 1 16.72 13.52 Q 15.552 15.552 13.536 16.72 A 8.725 8.725 0 0 1 9.786 17.857 A 10.672 10.672 0 0 1 8.96 17.888 Z M 8.96 16.096 A 7.357 7.357 0 0 0 11.174 15.772 A 6.497 6.497 0 0 0 12.56 15.152 Q 14.144 14.208 15.056 12.592 A 7.1 7.1 0 0 0 15.936 9.681 A 8.69 8.69 0 0 0 15.968 8.928 A 7.654 7.654 0 0 0 15.614 6.571 A 6.863 6.863 0 0 0 15.056 5.296 Q 14.144 3.68 12.56 2.736 A 6.729 6.729 0 0 0 9.58 1.815 A 8.205 8.205 0 0 0 8.96 1.792 A 7.171 7.171 0 0 0 6.468 2.218 A 6.674 6.674 0 0 0 5.376 2.736 Q 3.776 3.68 2.848 5.296 A 7.027 7.027 0 0 0 1.948 8.269 A 8.58 8.58 0 0 0 1.92 8.96 Q 1.92 10.976 2.848 12.592 Q 3.776 14.208 5.376 15.152 A 6.864 6.864 0 0 0 8.714 16.093 A 8.192 8.192 0 0 0 8.96 16.096 Z" vector-effect="non-scaling-stroke"/>';
+  }
+}
+class Klower_letter extends mojs.CustomShape {
+  getShape() {
+    return '<path d="M 0 24.032 L 0 0.96 A 1.188 1.188 0 0 1 0.035 0.663 A 0.831 0.831 0 0 1 0.272 0.256 A 0.933 0.933 0 0 1 0.834 0.006 A 1.25 1.25 0 0 1 0.96 0 Q 1.408 0 1.664 0.256 A 0.847 0.847 0 0 1 1.89 0.675 A 1.27 1.27 0 0 1 1.92 0.96 L 1.92 24.032 A 1.108 1.108 0 0 1 1.871 24.367 A 0.907 0.907 0 0 1 1.664 24.72 Q 1.408 24.992 0.96 24.992 Q 0.544 24.992 0.272 24.72 A 0.919 0.919 0 0 1 0.002 24.097 A 1.222 1.222 0 0 1 0 24.032 Z M 13.024 8.864 L 1.28 19.52 L 0.128 18.24 L 11.84 7.584 A 1.429 1.429 0 0 1 12.058 7.438 Q 12.264 7.328 12.464 7.328 Q 12.768 7.328 13.056 7.648 Q 13.344 7.968 13.344 8.288 A 0.636 0.636 0 0 1 13.213 8.672 Q 13.148 8.76 13.052 8.841 A 1.298 1.298 0 0 1 13.024 8.864 Z M 11.84 24.704 L 4.256 15.424 L 5.632 14.304 L 13.216 23.584 A 1.29 1.29 0 0 1 13.358 23.801 Q 13.457 23.993 13.457 24.186 A 0.754 0.754 0 0 1 13.456 24.224 Q 13.44 24.544 13.088 24.832 A 1.152 1.152 0 0 1 12.898 24.973 Q 12.715 25.079 12.528 25.079 A 0.676 0.676 0 0 1 12.432 25.072 A 0.837 0.837 0 0 1 12.009 24.878 A 1.21 1.21 0 0 1 11.84 24.704 Z" vector-effect="non-scaling-stroke"/>';
+  }
+}
+
+mojs.addShape("letterKcapital", K_letter);
+mojs.addShape("letterAlowercase", A_letter);
+mojs.addShape("letterMlowercase", M_letter);
+mojs.addShape("letterIlowercase", I_letter);
+mojs.addShape("letterLlowercase", L_letter);
+mojs.addShape("letterCcapital", C_letter);
+mojs.addShape("letterHlowercase", H_letter);
+mojs.addShape("letterOlowercase", O_letter);
+mojs.addShape("letterKlowercase", Klower_letter);
+
+const nameContainer = document.querySelector(".name-container");
+
+//kamil shapes
+const K = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterKcapital",
+  fill: "deeppink",
+  stroke: "deeppink",
+  scale: { 2: 1 },
+  y: { 0: 100 },
+  strokeWidth: { 20: 2 },
+  left: "20%",
+  top: "0",
+  duration: 700,
+}).play();
+const a = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterAlowercase",
+  fill: "none",
+  stroke: "white",
+  scale: { 0: 1 },
+  y: { 0: 107 },
+  strokeWidth: { 20: 2 },
+  left: "25%",
+  top: "0",
+  duration: 700,
+}).play();
+const m = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterMlowercase",
+  fill: "none",
+  stroke: "white",
+  scale: { 1: 1 },
+  y: { 0: 107.5 },
+  strokeWidth: { 20: 2 },
+  left: "31.5%",
+  top: "0",
+  duration: 700,
+}).play();
+const i = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterIlowercase",
+  fill: "none",
+  stroke: "white",
+  scale: { 0: 1 },
+  y: { 0: 102 },
+  strokeWidth: { 20: 2 },
+  left: "40%",
+  top: "-.5",
+  duration: 700,
+}).play();
+const l = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterLlowercase",
+  fill: "none",
+  stroke: "white",
+  scale: { 0: 1 },
+  y: { 0: 102 },
+  strokeWidth: { 20: 2 },
+  left: "42.5%",
+  top: "0",
+  duration: 700,
+}).play();
+
+//chomiak shapes
+const C = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterCcapital",
+  fill: "deeppink",
+  stroke: "deeppink",
+  scale: { 0: 1 },
+  y: { 0: 100 },
+  strokeWidth: { 20: 2 },
+  left: "48%",
+  top: "0",
+  duration: 700,
+}).play();
+const h = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterHlowercase",
+  fill: "none",
+  stroke: "white",
+  scale: { 0: 1 },
+  y: { 0: 100 },
+  strokeWidth: { 20: 2 },
+  left: "55%",
+  top: "0",
+  duration: 700,
+}).play();
+const o = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterOlowercase",
+  fill: "none",
+  stroke: "white",
+  scale: { 0: 1 },
+  y: { 0: 107.5 },
+  strokeWidth: { 20: 2 },
+  left: "61%",
+  top: "0",
+  duration: 700,
+}).play();
+const m2 = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterMlowercase",
+  fill: "none",
+  stroke: "white",
+  scale: { 0: 1 },
+  y: { 0: 107.5 },
+  strokeWidth: { 20: 2 },
+  left: "67%",
+  top: "0",
+  duration: 700,
+}).play();
+const i2 = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterIlowercase",
+  fill: "none",
+  stroke: "white",
+  scale: { 0: 1 },
+  y: { 0: 102 },
+  strokeWidth: { 20: 2 },
+  left: "76%",
+  top: "0",
+  duration: 700,
+}).play();
+const a2 = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterAlowercase",
+  fill: "none",
+  stroke: "white",
+  scale: { 0: 1 },
+  y: { 0: 107.5 },
+  strokeWidth: { 20: 2 },
+  left: "78%",
+  top: "0",
+  duration: 700,
+}).play();
+const k = new mojs.Shape({
+  parent: nameContainer,
+  shape: "letterKlowercase",
+  fill: "none",
+  stroke: "white",
+  scale: { 0: 1 },
+  y: { 0: 100 },
+  strokeWidth: { 20: 2 },
+  left: "84.5%",
+  top: "0",
+  duration: 700,
+}).play();
+
+K.el.style.zIndex = 12;
+a.el.style.zIndex = 12;
+m.el.style.zIndex = 12;
+i.el.style.zIndex = 12;
+l.el.style.zIndex = 12;
+C.el.style.zIndex = 12;
+h.el.style.zIndex = 12;
+o.el.style.zIndex = 12;
+m2.el.style.zIndex = 12;
+i2.el.style.zIndex = 12;
+a2.el.style.zIndex = 12;
+k.el.style.zIndex = 12;
+
+//---------------------------------------SWIRL SHAPES
+const shapeswirl = new mojs.ShapeSwirl({
+  parent: nameContainer,
+  fill: "white",
+  y: { 0: -150 },
+  duration: 3000,
+  top: "100",
+  left: "105",
+}).play();
+
+const circle = new mojs.Shape({
+  parent: nameContainer,
+  shape: "circle",
+  fill: "none",
+  stroke: "white",
+  isShowStart: true,
+  left: "85%",
+  top: "40%",
+  radius: { 0: 15 },
+  easing: "cubic.out",
+  strokeWidth: { 10: 0 },
+  duration: 1.5 * 400,
+  delay: 150,
+  className: "close-button",
+}).play();
+
+//---------------------------------------BOTTOM, TRANSITION TO WORK
+//---bottom right main circles
+const pinkCirc = new mojs.Shape({
+  shape: "circle",
+  // radiusX: { 0: 1300 },
+  // radiusY: { 0: 1300 },
+  radiusX: { 0: 2400 },
+  radiusY: { 0: 2400 },
+  left: "100%",
+  top: "0",
+  fill: "deeppink",
+  scale: { 1: 1 },
+  duration: 600,
+  isShowStart: false,
+  isShowEnd: false,
+  easing: "sin.in",
+});
+const whiteCirc = new mojs.Shape({
+  shape: "circle",
+  // radiusX: { 0: 1250 },
+  // radiusY: { 0: 1250 },
+  radiusX: { 0: 2300 },
+  radiusY: { 0: 2300 },
+  left: "100%",
+  top: "0",
+  fill: "white",
+  scale: { 1: 1 },
+  duration: 600,
+  delay: 50,
+  isShowStart: false,
+  isShowEnd: false,
+  easing: "sin.in",
+});
+const blackCirc = new mojs.Shape({
+  parent: nameContainer,
+  shape: "circle",
+  // radiusX: { 0: 1250 },
+  // radiusY: { 0: 1250 },
+  radiusX: { 0: 200 },
+  radiusY: { 0: 200 },
+  left: "40%",
+  top: "0%",
+  fill: "black",
+  scale: { 1: 1 },
+  duration: 500,
+  delay: 80,
+  isShowStart: true,
+  easing: "sin.in",
+});
+
+blackCirc.el.style.zIndex = 11;
+
+const smallBlackCircleBurst = new mojs.Burst({
+  parent: nameContainer,
+  left: "41%",
+  top: -100,
+  radius: { 4: 420 },
+  angle: 45,
+  count: 14,
+  children: {
+    delay: 500,
+    radius: 2.5,
+    fill: "#000",
+    scale: { 6: 0, easing: "quad.in" },
+    pathScale: [0.8, null],
+    degreeShift: [13, null],
+    duration: [500, 700],
+    easing: "quint.out",
+  },
+});
+
+smallBlackCircleBurst.el.style.zIndex = 11;
+
+const nav = document.querySelector(".nav");
+
+new mojs.Shape({
+  parent: nav,
+  shape: "circle",
+  isShowStart: true,
+  top: "0",
+  left: "100%",
+  scale: { 0: 2 },
+  duration: 1000,
+  delay: 500,
+  easing: "cubic.out",
+}).play();
+const workTransitionCircle = new mojs.Shape({
+  shape: "circle",
+  isShowStart: true,
+  top: "0",
+  left: "100%",
+  scale: { 0: 2.5 },
+  duration: 200,
+  //   delay: 2000,
+  easing: "cubic.out",
+});
+const workCircleBurst = new mojs.Shape({
+  left: "100%",
+  top: "0",
+  stroke: { deeppink: "deeppink" },
+  strokeWidth: { [2 * 10]: 0 },
+  fill: "none",
+  scale: { 0: 8 },
+  radius: 20,
+  duration: 400,
+  easing: "cubic.out",
+});
+
+pinkCirc.el.style.zIndex = 9;
+whiteCirc.el.style.zIndex = 10;
+
+let workTransitionHover = document.querySelector(".workTransition");
+//ANIMATE THE BUTTON
+workTransitionHover.addEventListener("mouseenter", () => {
+  workTransitionCircle.play();
+  workCircleBurst.play();
+});
+workTransitionHover.addEventListener("mouseleave", () => {
+  workTransitionCircle.playBackward();
+});
+
+//TRANSITION THE SCREEN
+let homeHtml = document.querySelector(".homeContainer");
+let workHtml = document.querySelector(".workContainer");
+let hideHomeHtml = () => {
+  setTimeout(() => {
+    homeHtml.style.display = "none";
+  }, 500);
+};
+let showWork = () => {
+  setTimeout(() => {
+    workHtml.style.display = "flex";
+  }, 500);
+};
+
+workTransitionHover.addEventListener("click", () => {
+  pinkCirc.play();
+  whiteCirc.play();
+  blackCirc.play();
+  smallBlackCircleBurst.play();
+  hideHomeHtml();
+  showWork();
+});
